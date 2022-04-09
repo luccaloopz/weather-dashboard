2faf23511fcd2203f17e4ad49f264e10b109018e
luccaloopz <luccamartins2012@gmail.com>
2022-04-09 16:30:42 -0700

    once user clicks search button, current weather data is displayed.

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -4,12 +4,27 @@
 //second api call: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} (to get actual 5 day forcast)
 
 const searchBtn = document.querySelector("#searchBtn");
+const currentDateEl = document.querySelector("#current-date");
+const cityNameEl = document.querySelector("#current-cityname");
+const currentTempEl = document.querySelector("#current-temp");
+const currentHumidityEl = document.querySelector("#current-humidity");
+const currentWindspeedEl = document.querySelector("#current-windspeed");
+const currentUVIEl = document.querySelector("#current-UVI");
 
 searchBtn.addEventListener("click", firstAPICALL);
 
 function firstAPICALL() {
-    const cityName = document.querySelector("#searchInput").value;
+    let cityName = document.querySelector("#searchInput").value;
     const firstApiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=684b85a12af0b7eb0522d2b94d7fdee3`;
+    cityName = cityName.split(" ");
+    for (let i = 0; i < cityName.length; i++) {
+        cityName[i] = cityName[i][0].toUpperCase() + cityName[i].slice(1);
+    };
+    cityName = cityName.join(" ");
+    cityNameEl.textContent = cityName + " ";
+
+    const currentDate = moment().format("dddd, MMMM Do YYYY");
+    currentDateEl.textContent = currentDate
     
     fetch(firstApiUrl).then(function(response) {
         return response.json();
@@ -22,17 +37,26 @@ function firstAPICALL() {
 };
 
 function secondAPICALL(lat, lon) {
-    const secondApiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=684b85a12af0b7eb0522d2b94d7fdee3`;
+    const secondApiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=684b85a12af0b7eb0522d2b94d7fdee3`;
 
     fetch(secondApiURL).then(function(response) {
         return response.json();
     }).then(function(data) {
-        console.log(data);
-        displayingCityData(data);
+        displayingCurrentCityData(data);
     });
 };
 
-function displayingCityData(data) {
+function displayingCurrentCityData(data) {
     //this will take the "data" object, parse through it to get specific data and append them to the text content of the html elements
-    //you might need to pass certain data through other functions that call other APIs for conversion. For example, the temp's default is in kelvin, meaning you'll need to use the temp conversion API
-}
+    const currentTemp = data.current.temp;
+    currentTempEl.textContent = currentTemp + "℉";
+
+    const currentHumidity = data.current.humidity;
+    currentHumidityEl.textContent = currentHumidity + "%";
+
+    const currentWindspeed = data.current.wind_speed;
+    currentWindspeedEl.textContent = currentWindspeed + "MPH";
+
+    const currentUVI = data.current.uvi;
+    currentUVIEl.textContent = currentUVI;
+};
