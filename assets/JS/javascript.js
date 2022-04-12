083beb35d3183da6c76a212c963d9bc8f12ca1bc
luccaloopz <luccamartins2012@gmail.com>
2022-04-11 21:33:05 -0700

    cleaned up js file.

--- assets/JS/javascript.js
+++ assets/JS/javascript.js
@@ -1,5 +1,3 @@
-//api key: 684b85a12af0b7eb0522d2b94d7fdee3
-
 // api call: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key} (getting the lat and long)
 //second api call: https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key} (to get actual 5 day forcast)
 
